19891863abae7804608ce316d2bd9acbf54ac598
jaime <jamesowiti24@gmail.com>
2024-07-05 11:49:31 +0300

    Added grids and mouseover effect

---
+++ script.js
@@ -0,0 +1,7 @@
+let gridBox = document.getElementsByClassName("grid");
+
+function hoverGrid() {
+    //document.getElementsByClassName("grid").style.backgroundColor = "red";
+    document.getElementById("grids").style.backgroundColor = "blue";
+  console.log("hovered");
+}
